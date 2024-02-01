a991f5210a748df8e147e9e0a4300d41b0736253
jeesmariyatony123 <jeestony123@gmail.com>
2024-02-01 08:42:22 +0530

    recipeapi v1

---
+++ script.js
@@ -0,0 +1,9 @@
+const searchicon1=document.querySelector('#seachicon1')
+const srchicon1=document.querySelector('#srchicon1')
+const search1=document.querySelector('#searchinput1')
+
+
+searchicon1.addEventListener('click', function(){
+    search1.style.display='flex'
+    searchicon1.style.display='none'
+})
